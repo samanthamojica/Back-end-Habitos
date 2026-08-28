ef881f71059bb10a114599a6b634acc51641d386
luisz2310 <luisz2310@hotmail.com>
2026-08-28 17:01:19 -0500

    Init project

---
+++ src/main/java/com/samantha/habitos/HabitosApplication.java
@@ -0,0 +1,13 @@
+package com.samantha.habitos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HabitosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HabitosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/samantha/habitos/HabitosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samantha.habitos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabitosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
